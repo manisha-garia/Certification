9f3b53f9e75c2e063e25e19719612e56177a1f0f
manishagaria <manishagaria@hotmail.com>
2017-05-06 21:48:12 +0530

    Sample program

---
+++ Practice/src/main/java/manisha/Practice1.java
@@ -0,0 +1,11 @@
+package main.java.manisha;
+
+/**
+ * Created by Manisha Garia on 06-05-2017.
+ */
+public class Practice1
+{
+    public static void main(String args[]){
+        System.out.println("hello km");
+    }
+}
